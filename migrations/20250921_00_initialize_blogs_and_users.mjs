985f29f1b8d412d8a1ae4388cd2187b85f0f7c39
Gianluca-Sc <scibettagianluca@gmail.com>
2025-09-21 11:58:16 +0200

    Add exercise 13.17

---
+++ migrations/20250921_00_initialize_blogs_and_users.mjs
@@ -0,0 +1,77 @@
+import { DataTypes, Sequelize } from "sequelize";
+
+export const up = async ({ context: queryInterface }) => {
+  try {
+    await queryInterface.createTable("blogs", {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      author: {
+        type: DataTypes.TEXT,
+      },
+      url: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      title: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      likes: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    });
+    await queryInterface.createTable("users", {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      username: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    });
+
+    await queryInterface.addColumn("blogs", "user_id", {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: "users", key: "id" },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const down = async ({ context: queryInterface }) => {
+  await queryInterface.dropTable("blogs");
+  await queryInterface.dropTable("users");
+};
--- util/db.js
+++ util/db.js
@@ -1,5 +1,6 @@
 import { Sequelize } from "sequelize";
 import { DATABASE_URL } from "./config.js";
+import { SequelizeStorage, Umzug } from "umzug";
 
 export const sequelize = new Sequelize(DATABASE_URL, {
   dialectOptions: {
@@ -10,9 +11,27 @@ export const sequelize = new Sequelize(DATABASE_URL, {
   },
 });
 
+const runMigrations = async () => {
+  const migrator = new Umzug({
+    migrations: {
+      glob: "migrations/*.{js,mjs}",
+    },
+    storage: new SequelizeStorage({ sequelize, tableName: "migrations" }),
+    context: sequelize.getQueryInterface(),
+    logger: console,
+  });
+  console.log("migraation");
+
+  const migrations = await migrator.up();
+  console.log("Migrations up to date", {
+    files: migrations.map((mig) => mig.name),
+  });
+};
+
 export const connectToDatabase = async () => {
   try {
     await sequelize.authenticate();
+    await runMigrations();
     console.log("connected to the database");
   } catch (err) {
     console.log("failed to connect to the database");
